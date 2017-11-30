1d6f5372f19f12f001e470bd1bfe73182149d199
redshiftzero <jen@freedom.press>
2017-11-30 14:09:33 -0800

    Fix #2647: Make sure source interface logout preserves user locale

--- securedrop/source_app/main.py
+++ securedrop/source_app/main.py
@@ -221,8 +221,12 @@ def make_blueprint(config):
         if logged_in():
             msg = render_template('logout_flashed_message.html')
 
-            # clear the session after we render the message so it's localized
+            # Clear the session after we render the message so it's localized
+            # If a user specified a locale, save it and restore it
+            default_locale = getattr(config, 'DEFAULT_LOCALE', 'en_US')
+            user_locale = session.get('locale', default_locale)
             session.clear()
+            session['locale'] = user_locale
 
             flash(Markup(msg), "important hide-if-not-tor-browser")
         return redirect(url_for('.index'))
